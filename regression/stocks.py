8da799e81750fbf6aaa8741325f12682db3c41a3
marcus <ms2asdk@gmail.com>
2017-04-01 13:53:35 -0700

    train

--- regression/stocks.py
+++ regression/stocks.py
@@ -28,5 +28,8 @@ y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
 
-print(df.head())
+print(accuracy)
