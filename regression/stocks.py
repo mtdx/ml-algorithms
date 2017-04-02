517db5494afb0b5ac2ddff540fa839645e66821c
marcus <ms2asdk@gmail.com>
2017-04-02 07:39:56 -0700

    plot the results

--- regression/stocks.py
+++ regression/stocks.py
@@ -2,6 +2,11 @@ import pandas as pd
 import quandl
 import math
 import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
@@ -20,7 +25,6 @@ forecast_o = int(math.ceil(0.01 * len(df)))
 # set label
 df['label'] = df[forecast_c].shift(-forecast_o)
 
-
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
 X = X[:-forecast_o]
@@ -33,7 +37,24 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
-
 forecast_set = clf.predict(X_lately)
 
-print(forecast_set, accuracy, forecast_o)
+# print(forecast_set, accuracy, forecast_o)
+
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
