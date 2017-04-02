e1109fdee73f9bdcd173196015c6ae5593bb9fc6
marcus <ms2asdk@gmail.com>
2017-04-02 12:18:31 -0700

    plot the regression line

--- regression/algorithm.py
+++ regression/algorithm.py
@@ -1,20 +1,31 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
 
-plt.scatter(xs, ys)
-plt.show()
+
+# plt.scatter(xs, ys)
+# plt.show()
 
 
-def best_fit_slope(xs, ys):
+def best_fit_slope_and_intercept(xs, ys):
     m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
-         ((mean(xs) ** 2) - mean(xs ** 2)))
-    return m
+         ((mean(xs) * mean(xs)) - mean(xs * xs)))
+
+    b = mean(ys) - m * mean(xs)
+
+    return m, b
 
 
-m = best_fit_slope(xs, ys)
+m, b = best_fit_slope_and_intercept(xs, ys)
+regression_line = [(m * x) + b for x in xs]
 
-print(m)
+plt.scatter(xs, ys, color='#003F72', label='data')
+plt.plot(xs, regression_line, label='regression line')
+plt.legend(loc=4)
+plt.show()
