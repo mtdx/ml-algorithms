4adf4c16f9ee921ed8c9d652274948cf7ca5ebfe
marcus <ms2asdk@gmail.com>
2017-04-10 01:59:47 -0700

    handle non numerical data

--- clustering/hierarchical-clustering-eg.py
+++ clustering/hierarchical-clustering-eg.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
+
+style.use('ggplot')
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn import preprocessing, cross_validation
+import pandas as pd
 
 style.use('ggplot')
 
@@ -28,3 +32,37 @@ for i in range(len(X)):
     plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=10)
 plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", s=150, linewidths=5, zorder=10)
 plt.show()
+
+df = pd.read_excel('../datasets/titanic.xls')
+# print(df.head())
+df.drop(['body', 'name'], 1, inplace=True)
+df.convert_objects(convert_numeric=True)
+df.fillna(0, inplace=True)
+
+
+# print(df.head())
+
+def handle_non_numerical_data(df):
+    columns = df.columns.values
+    for column in columns:
+        text_digit_vals = {}
+
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+            column_contents = df[column].values.tolist()
+            unique_elements = set(column_contents)
+            x = 0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x += 1
+
+            df[column] = list(map(convert_to_int, df[column]))
+
+    return df
+
+
+df = handle_non_numerical_data(df)
+print(df.head())
