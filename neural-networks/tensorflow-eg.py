7b988cda80a5e62d98e24721b36e33ad5456ecec
marcus <ms2asdk@gmail.com>
2017-04-15 11:02:22 -0700

    use sentiment featuresets

--- neural-networks/tensorflow-eg.py
+++ neural-networks/tensorflow-eg.py
@@ -1,74 +1,82 @@
+from create_sentiment_featuresets import create_feature_sets_and_labels
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
+import pickle
+import numpy as np
 
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+train_x, train_y, test_x, test_y = create_feature_sets_and_labels('/path/to/pos.txt', '/path/to/neg.txt')
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 1500
+n_nodes_hl2 = 1500
+n_nodes_hl3 = 1500
 
-n_classes = 10
+n_classes = 2
 batch_size = 100
+hm_epochs = 10
 
-x = tf.placeholder('float', [None, 784])
+x = tf.placeholder('float')
 y = tf.placeholder('float')
 
+hidden_1_layer = {'f_fum': n_nodes_hl1,
+                  'weight': tf.Variable(tf.random_normal([len(train_x[0]), n_nodes_hl1])),
+                  'bias': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
+hidden_2_layer = {'f_fum': n_nodes_hl2,
+                  'weight': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                  'bias': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+hidden_3_layer = {'f_fum': n_nodes_hl3,
+                  'weight': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                  'bias': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+output_layer = {'f_fum': None,
+                'weight': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                'bias': tf.Variable(tf.random_normal([n_classes])), }
+
+
+# Nothing changes
 def neural_network_model(data):
-    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
-
-    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
-
-    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-
-    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases': tf.Variable(tf.random_normal([n_classes])), }
-
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weight']), hidden_1_layer['bias'])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weight']), hidden_2_layer['bias'])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weight']), hidden_3_layer['bias'])
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+    output = tf.matmul(l3, output_layer['weight']) + output_layer['bias']
 
     return output
 
 
 def train_neural_network(x):
     prediction = neural_network_model(x)
-    # OLD VERSION:
-    # cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
-    # NEW:
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
-    hm_epochs = 10
     with tf.Session() as sess:
-        # OLD:
-        # sess.run(tf.initialize_all_variables())
-        # NEW:
-        sess.run(tf.global_variables_initializer())
+        sess.run(tf.initialize_all_variables())
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples / batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+            i = 0
+            while i < len(train_x):
+                start = i
+                end = i + batch_size
+                batch_x = np.array(train_x[start:end])
+                batch_y = np.array(train_y[start:end])
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                              y: batch_y})
                 epoch_loss += c
+                i += batch_size
 
-            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
-
+            print('Epoch', epoch + 1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
+        print('Accuracy:', accuracy.eval({x: test_x, y: test_y}))
 
 
 train_neural_network(x)
